e3fee944237229fb676f2e8c140154442228d95e
Emek <20749586+eliasmekuria@users.noreply.github.com>
2025-02-13 16:27:47 -0500

    Add form submission document.js

--- document.js
+++ document.js
@@ -1,3 +1,31 @@
+
+ document.getElementById("contactForm").addEventListener("submit", async function (event) {
+        event.preventDefault();
+
+        const formData = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            reason: document.getElementById("reason").value
+        };
+
+        const response = await fetch("https://script.google.com/macros/s/AKfycbyd_qTqQNoF4sQW4vN0fCPAuTw9C9KQ7sbHwyGHSOjtZAIDWGQ7aADi_nzkq1Fdvfu4/exec", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+            alert("Form submitted successfully!");
+            this.reset();
+        } else {
+            alert("Error submitting form.");
+        }
+    });
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const lines = document.querySelectorAll(".slide-up");
 
